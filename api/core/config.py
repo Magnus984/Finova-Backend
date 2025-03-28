48d651a23207980e0513e776f7c6ef7b98180d48
Magnus984 <nharnharyhaw984@gmail.com>
2025-03-28 11:57:50 +0000

    feat: enable DEBUG mode in settings

--- api/core/config.py
+++ api/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DEBUG: bool = True
     PROJECT_TITLE: str = "finova-backend"
     API_V1_STR: str = "/api/v1"
     DB_HOST: str
