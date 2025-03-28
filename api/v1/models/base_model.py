67d7b9d32ac95a552acc840d052a70b3d011368e
Magnus984 <nharnharyhaw984@gmail.com>
2025-03-28 01:53:07 +0000

    fix: correct datetime import and update default values in BaseModel

--- api/v1/models/base_model.py
+++ api/v1/models/base_model.py
@@ -2,16 +2,16 @@
 Base Model module
 """
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from mongoengine import Document, StringField, DateTimeField, UUIDField
 
 class BaseModel(Document):
     """
     BaseModel class
     """
-    id = UUIDField(primary_key=True, default= lambda: uuid.uuid4())
-    created_at = DateTimeField(default= lambda: datetime.now(datetime.timezone.utc))
-    updated_at = DateTimeField(defualt= lambda: datetime.now(datetime.timezone.utc))
+    id = UUIDField(primary_key=True, default=lambda: uuid.uuid4())
+    created_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    updated_at = DateTimeField(default=lambda: datetime.now(timezone.utc))
 
     meta = {
         'abstract': True
