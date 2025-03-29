559506f56003e87ba513afcd9b405543e31bf130
Magnus984 <nharnharyhaw984@gmail.com>
2025-03-29 15:12:10 +0000

    feat: add user and company models with relationships and update base model ID type

---
+++ api/v1/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import User
+from .company import Company
+
+__all__ = ['User', 'Company']
--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -1,21 +1,29 @@
 from .base_model import BaseModel
-from datetime import datetime
+from datetime import datetime, timezone
 from mongoengine import (
     StringField, EmailField, URLField,
-    BooleanField, DateTimeField
+    BooleanField, DateTimeField, ReferenceField
 )
+import mongoengine
+from .company import Company
 
 class User(BaseModel):
     email = EmailField(required=True, unique=True)
     first_name = StringField()
     last_name = StringField()
+    password = StringField()
     avatar_url = URLField()
     is_active = BooleanField(default=False)
     is_superadmin = BooleanField(default=False)
     is_deleted = BooleanField(default=False)
     is_verified = BooleanField(default=False)
-    last_login = DateTimeField(defualt= lambda: datetime.now(datetime.timezone.utc))
+    last_login = DateTimeField(default=lambda: datetime.now(timezone.utc))
+    
+    company = ReferenceField("Company", reverse_delete_rule=mongoengine.NULLIFY)
 
     meta = {
-        "collection": "users"
+        "collection": "users",
+        "indexes": [
+            "email",
+        ]
     }
